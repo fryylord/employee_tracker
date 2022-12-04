e39d8f463198557d162bd62698ef3f02983578bf
fryylord <rob@mchays.com>
2022-12-03 18:45:47 -0600

    basic prompt structure created in index.js

--- index.js
+++ index.js
@@ -2,3 +2,58 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const init = require('init');
 require('console.table');
+
+function mainPrompt() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'Add employee',
+                'Update employee role',
+                'View all roles',
+                'Add a role',
+                'View all departments',
+                'Add a department',
+                'Quit'
+            ]
+        })
+        .then(answer => {
+            console.log('answer', answer);
+            switch (answer.action) {
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+
+                case 'Add employee':
+                    addEmployee();
+                    break;
+
+                case 'Update employee role':
+                    updateEmployeeRole();
+                    break;
+
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+
+                case 'Add a role':
+                    addRole();
+                    break;
+
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+
+                case 'Add a department':
+                    addDepartment();
+                    break;
+
+                case promptMessages.exit:
+                    connection.end();
+                    break;
+            }
+        });
+}
