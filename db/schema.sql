02aa59db778085b5648e1709f5f347607a9dd1ae
fryylord <rob@mchays.com>
2022-12-03 20:49:15 -0600

    created schema for tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,20 @@
-DROP DATABASE IF EXISTS employeesDB;
-CREATE DATABASE employeesDB;
-USE employeesDB;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary DECIMAL(10.3) NULL,
-  department_id INT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10.3) NOT NULL,
+  department INT,
+  FOREIGN KEY (department)
+  REFERENCES department(id),
   PRIMARY KEY (id)
 );
 
@@ -20,7 +22,8 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
+  role INT,
+  FOREIGN KEY (role)
+  REFERENCES role(id),
   PRIMARY KEY (id)
 );
