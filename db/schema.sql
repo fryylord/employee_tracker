f1429f54050bee830944030e38069c149001a3ec
fryylord <rob@mchays.com>
2022-12-04 13:39:35 -0600

    fixed lables in schema/seed

--- db/schema.sql
+++ db/schema.sql
@@ -12,9 +12,8 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department INT,
-  FOREIGN KEY (department)
-  REFERENCES department(id),
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id),
   PRIMARY KEY (id)
 );
 
@@ -22,7 +21,7 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
-  role INT, FOREIGN KEY (role) REFERENCES role(id),
-  managerId INT, FOREIGN KEY (managerId) REFERENCES employee(id),
+  role_id INT, FOREIGN KEY (role_id) REFERENCES role(id),
+  manager_id INT, FOREIGN KEY (manager_Id) REFERENCES employee(id),
   PRIMARY KEY (id) 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -9,30 +9,30 @@ VALUES ("Accounting");
 INSERT INTO department (name)
 VALUES ("Administration");
 
-INSERT INTO role (title, salary, department)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Call Center Operator", 100000, 1);
-INSERT INTO role (title, salary, department)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Floor Manager", 125000, 2);
-INSERT INTO role (title, salary, department)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Assistant", 100000, 3);
-INSERT INTO role (title, salary, department)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Accountant", 125000, 3);
-INSERT INTO role (title, salary, department)
+INSERT INTO role (title, salary, department_id)
 VALUES ("President", 200000, 4);
 
-INSERT INTO employee (first_name, last_name, role, managerId)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Kefka", "Palazzo", 5, null);
-INSERT INTO employee (first_name, last_name, role, managerId)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Terra", "Branford", 2, 1);
-INSERT INTO employee (first_name, last_name, role, managerId)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Celes", "Chere", 3, null);
-INSERT INTO employee (first_name, last_name, role, managerId)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Edgar", "Figaro", 4, 3);
-INSERT INTO employee (first_name, last_name, role, managerId)
-VALUES ("Cyan", "Garamonde", 5, 5);
-INSERT INTO employee (first_name, last_name, role, managerId)
-VALUES ("Setzer", "Gabbiani", 2, null);
-INSERT INTO employee (first_name, last_name, role, managerId)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Cyan", "Garamonde", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Setzer", "Gabbiani", 2, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Sabin", "Figaro", 4, 7);
-INSERT INTO employee (first_name, last_name, role, managerId)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Strago", "Magus", 1, 2);
