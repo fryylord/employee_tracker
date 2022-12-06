de61e114743ce800166a53f7ae91b27976322157
fryylord <rob@mchays.com>
2022-12-05 19:50:47 -0600

    potential role adding function

--- index.js
+++ index.js
@@ -231,3 +231,35 @@ const viewAllRoles = () => {
     if (error) throw error;
     console.table(response);
     mainPrompt();})};
+
+function addRole() { 
+  connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, res) {
+    inquirer.prompt([
+        {
+          name: "Title",
+          type: "input",
+          message: "What is the roles Title?"
+        },
+        {
+          name: "Salary",
+          type: "input",
+          message: "What is the Salary?"
+
+        } 
+    ]).then(function(res) {
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: res.Title,
+              salary: res.Salary,
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                mainPrompt();
+            }
+        )
+
+    });
+  });
+}
